d9fff8c6b89389f0beb2cde34e86fe0cea030340
Nikolas Raposo Martins Vieira <nikolasraposo@gmail.com>
2024-11-19 12:14:32 -0300

    Build Effects
    Criado efeito quando uma torre é construída

--- Assets/_Scripts/BuildManager.cs
+++ Assets/_Scripts/BuildManager.cs
@@ -14,19 +14,20 @@ public class BuildManager : MonoBehaviour
     }
     public GameObject standardTowerPrefab;
     public GameObject anotherTowerPrefab;
+    public GameObject buildEffct;
     private TowerBlueprint towerToBuild;
     public bool CanBuild { get {return towerToBuild != null; } }
     public bool HasMoney { get {return StatsManager.Money >= towerToBuild.cost; } }
     public void BuildTowerOn(NodeController node) {
         if (StatsManager.Money < towerToBuild.cost)
         {
-            Debug.Log ("Not Money");
             return;
         }
         StatsManager.Money -= towerToBuild.cost;
         GameObject tower = (GameObject)Instantiate(towerToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.tower = tower;
-        Debug.Log("Current Money: "+StatsManager.Money);
+        GameObject effect = (GameObject)Instantiate(buildEffct, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
     }
     public void SelectTowerToBuild(TowerBlueprint tower) {
         towerToBuild = tower;
